6cb7d5809071e9688183236e099e57a793318bed
Renan Caldeira Nunes <renan.rcn@hotmail.com>
2020-01-15 18:48:58 -0300

    Initial commit

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,31 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+
+module.exports = {
+    async index(request, response) {
+        // Buscar todos devs num raio de 10km
+        // Filtrar por tecnologia
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        // Filtra os devs que tiverem as tecnologias procuradas dentro
+        // de techsArray
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                },
+            },
+        });
+
+        return response.json({ devs });
+    }
+}
